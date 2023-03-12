29076cf1139b1d37f017c6adb3ffb4acad543d86
mdasan <624.mdasan@gmail.com>
2023-03-12 10:42:50 +0900

    calcを削除

--- src/CalcAmount.php
+++ src/CalcAmount.php
@@ -2,58 +2,13 @@
 
 namespace src;
 
-use src\AmountAdjustmentFactory;
 use src\Spec\People\Customer;
 use src\Spec\People\AdultPeople;
 use src\Spec\People\ChildPeople;
 
-//require 'src/Core/PeopleInterface.php';
-
 class CalcAmount
 {
     public function calc(int $adultCount, int $childCount, bool $isSpecial, \DateTimeImmutable $dateTime): int
-    {
-        if ($isSpecial) {
-            $adultAmount = 600;
-        } else {
-            $adultAmount = 1000;
-        }
-        $adultAmountEvening = $adultAmount - 100;
-        $adultAmountHoliday = $adultAmount + 200;
-
-        if ($isSpecial) {
-            $childAmount = 400;
-        } else {
-            $childAmount = 500;
-        }
-        $childAmountEvening = $childAmount - 100;
-        $childAmountHoliday = $childAmount + 200;
-
-        $dayOfWeek = $dateTime->format('w');
-
-        $evening = $dateTime->setTime(17, 0, 0);
-        $isEvening = ($dateTime >= $evening) && !($dayOfWeek == 0 || $dayOfWeek == 6);
-
-        $isHoliday = $dayOfWeek == 0 || $dayOfWeek == 6;
-
-        $subTotal =  ($adultCount * $adultAmount) + ($childCount * $childAmount);
-
-        $totalAmount = $subTotal;
-
-        if ($isEvening) {
-            $totalAmount =  ($adultCount * $adultAmountEvening) + ($childCount * $childAmountEvening);
-        } elseif ($isHoliday) {
-            $totalAmount =  ($adultCount * $adultAmountHoliday) + ($childCount * $childAmountHoliday);
-        }
-
-        if (($adultCount + ($childCount * 0.5)) >= 10) {
-            $totalAmount = $totalAmount - round($totalAmount * 0.1);
-        }
-
-        return $totalAmount;
-    }
-
-    public function provisionalCalc(int $adultCount, int $childCount, bool $isSpecial, \DateTimeImmutable $dateTime): int
     {
         $amountType = '';
         if ($isSpecial) {
